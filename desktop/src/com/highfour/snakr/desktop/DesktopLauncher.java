caaef178f58ac583936892fa1dbf68f0259a9513
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-05-01 14:11:14 +0200

    added empty libgdx project

---
+++ desktop/src/com/highfour/snakr/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.highfour.snakr.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.highfour.snakr.Snakr;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Snakr(), config);
+	}
+}
