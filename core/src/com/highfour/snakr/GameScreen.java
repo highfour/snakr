552f218bf96c443d01ca50bde769b374c0b55e9c
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-05-02 00:47:42 +0200

    kollisionen werden gezaehlt und spieler zurueckgesetzt

--- core/src/com/highfour/snakr/GameScreen.java
+++ core/src/com/highfour/snakr/GameScreen.java
@@ -3,6 +3,7 @@ package com.highfour.snakr;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,14 +11,14 @@ import com.badlogic.gdx.math.Rectangle;
 
 public class GameScreen implements Screen {
 
-    SpriteBatch batch;
     Texture img;
-    int direction, speed;
+    int direction, speed, collisions;
     Rectangle highfour;
+    Snakr game;
 
     public GameScreen(final Snakr game) {
 
-        batch = new SpriteBatch();
+        this.game = game;
         img = new Texture("highfour.png");
 
         highfour = new Rectangle();
@@ -32,13 +33,22 @@ public class GameScreen implements Screen {
 
     @Override
     public void render (float delta) {
+        // set background color to white
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
-        batch.draw(img, highfour.x, highfour.y, 200, 200);
-        batch.end();
+        // send everything to be rendered
+        game.batch.begin();
+        // draw actor
+        game.batch.draw(img, highfour.x, highfour.y, 200, 200);
+        // change font color
+        game.font.setColor(Color.BLACK);
+        // draw collisions text in upper right hand corner
+        game.font.draw(game.batch, "Collisions: " + collisions, 10, 590);
+        game.batch.end();
 
+        // the four possible directions
+        // FIXME: holding two keys should be impossible
         switch(direction){
             case 0:
                 highfour.y += speed * Gdx.graphics.getDeltaTime();
@@ -56,7 +66,7 @@ public class GameScreen implements Screen {
                 System.out.println("wrong direction");
         }
 
-
+        // stop the player from turning 180 degrees
         if (direction % 2 == 0) {
             if(Gdx.input.isKeyPressed(Keys.LEFT)) direction = 3;
             if(Gdx.input.isKeyPressed(Keys.RIGHT)) direction = 1;
@@ -65,11 +75,31 @@ public class GameScreen implements Screen {
             if(Gdx.input.isKeyPressed(Keys.UP)) direction = 0;
         }
 
-        if(highfour.x < 0) highfour.x = 0;
-        if(highfour.x > 800 - 200) highfour.x = 800 - 200;
-        if(highfour.y < 0) highfour.y = 0;
-        if(highfour.y > 600 - 200) highfour.y = 600 - 200;
+        // keep the actor within the game boundaries and check for collisions
+        if(highfour.x < 0) {
+            highfour.x = 0;
+            resetPlayer(highfour);
+        }
+        if(highfour.x > 800 - 200) {
+            highfour.x = 800 - 200;
+            resetPlayer(highfour);
+        }
+        if(highfour.y < 0) {
+            highfour.y = 0;
+            resetPlayer(highfour);
+        }
+        if(highfour.y > 600 - 200) {
+            highfour.y = 600 - 200;
+            resetPlayer(highfour);
+        }
 
+    }
+
+    // reset the player to the middle of the screen and count up a collision
+    public void resetPlayer(Rectangle player) {
+        collisions++;
+        player.x = 800 / 2 - 200 / 2;
+        player.y = 600 / 2 - 200 / 2;
     }
 
     @Override
@@ -99,7 +129,6 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-        batch.dispose();
         img.dispose();
     }
 }
