3b307455004d2488073957574978bca8982d2d5b
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-05-05 17:08:11 +0200

    1. Deploy to Android & HTML - 2. ??? - 3. PROFIT!

---
+++ html/src/com/highfour/snakr/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.highfour.snakr.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.highfour.snakr.Snakr;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(800, 600);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Snakr();
+        }
+}
